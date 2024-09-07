8ad689f645c3fff2fae5da6a78ecf4df32a171e5
Saad Aziz <saad.aziz101@gmail.com>
2024-09-08 00:37:44 +0100

    progress

--- syntax/Package.swift
+++ syntax/Package.swift
@@ -11,7 +11,7 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .executableTarget(
-            name: "MyCLI",
+            name: "main",
             dependencies: [
                 .product(name: "Figlet", package: "example-package-figlet"),
             ],
