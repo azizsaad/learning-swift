76fb5934cf8d95a00be75439683226b82c63c259
Saad Aziz <saad.aziz101@gmail.com>
2024-09-08 13:10:18 +0100

    progress

--- syntax/Sources/main.swift
+++ syntax/Sources/main.swift
@@ -1,8 +1,18 @@
-import Figlet
+print("hello world")
 
-@main
-struct FigletTool {
-  static func main() {
-    Figlet.say("Hello, Swift!")
-  }
-}
+var alpha: Int = 42
+alpha = 50
+
+print(alpha)
+
+let beta: Float = 4 // 32 bit memory
+let gamma: Double = 2 // 64 bit memory
+
+print(beta)
+print(gamma)
+
+let label = "The width is "
+let width = 94
+let widthLabel = label + String(width)
+
+print(widthLabel)
